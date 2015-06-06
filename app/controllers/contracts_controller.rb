f9e5e9d8cecd34daa1f9237e2f5a9c33661fd384
Shane Carr <shane.carr@sitesignals.com>
2015-06-06 16:19:44 +0000

    partials created

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -13,7 +13,35 @@ class ContractsController < ApplicationController
     end
     
     def create
-       binding.pry 
+       @contract = Contract.new(contract_params)
+       @contract.user = User.find(3)
+       
+       if @contract.save
+           flash[:success] = "Your Contract has been created."
+           redirect_to contracts_path
+       else
+           render :new
+       end
     end
+    
+    def edit
+        @contract = Contract.find(params[:id])
+    end
+    
+    def update
+        @contract = Contract.find(params[:id])
+        if @contract.update(contract_params)
+           flash[:success] = "Your Contract was updated successfully!"
+           redirect_to contract_path(@contract)
+        else
+            render :edit
+        end
+    end
+    
+    private #private params for the create method
+        
+        def contract_params
+            params.require(:contract).permit(:desc, :region, :value, :saving, :supplier, :contact, :dept, :download)
+        end
     
 end
