9d4e6823946996c21d99d43bdee68d9fbb3ddbd6
Shane Carr <shane.carr@sitesignals.com>
2015-06-19 13:18:02 +0000

    user profile with contracts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,13 +21,25 @@ def create
 end
 
 def edit
-    
+    @user = User.find(params[:id])
 end
 
 
 def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+        flash[:success] = "Your profile has been updated successfully."
+        redirect_to contracts_path
+    else
+        render 'edit'    
+    end
     
 end
+
+def show
+    @user = User.find(params[:id])    
+end
+
 
 private
 
